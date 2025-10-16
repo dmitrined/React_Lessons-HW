d1f532fe6013bd478f8de04b6671b55a4c70aacf
dmitrined <dm.nedioglo@gmail.com>
2025-10-16 21:56:01 +0200

    ok

--- Lesson_3/vite-project/src/App.tsx
+++ Lesson_3/vite-project/src/App.tsx
@@ -1,7 +1,6 @@
 
 import type { JSX } from 'react'
 import './App.css'
-import CarShop from './components/CarShop/CarShop'
 import Counter from './components/Counter/Counter'
 import Sandwich from './components/Sandwich/Sandwich'
 import Alcohol from './components/Alcohol/Alcohol'
@@ -10,7 +9,7 @@ export default function App() : JSX.Element {
 
   return (
    <div> 
-      <CarShop />
+    
       <Counter />
        <Sandwich />
          <Alcohol />
