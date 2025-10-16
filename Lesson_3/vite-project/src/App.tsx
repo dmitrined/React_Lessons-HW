0547a830b2ad0b49cfb4e7413c63768a81c31396
dmitrined <dm.nedioglo@gmail.com>
2025-10-16 18:56:23 +0200

    ok

---
+++ Lesson_3/vite-project/src/App.tsx
@@ -0,0 +1,15 @@
+
+import type { JSX } from 'react'
+import './App.css'
+import CarShop from './components/CarShop/CarShop'
+
+export default function App() : JSX.Element {
+
+  return (
+   <div> 
+      <CarShop />
+   </div>
+  )
+}
+
+
