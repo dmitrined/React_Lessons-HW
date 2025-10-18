dedb329f58e46310781bb2ecce95397a0e836f84
dmitrined <dm.nedioglo@gmail.com>
2025-10-18 14:30:15 +0200

    ok

--- L2_hw/vite-project/src/components/ProfileCard/ProfileCard.tsx
+++ L2_hw/vite-project/src/components/ProfileCard/ProfileCard.tsx
@@ -1,18 +1,23 @@
-interface Profile {
-  avatarUrl: string;
-  firstName: string;
-  lastName: string;
-  occupation: string;
-  hobby: string;
-}
+import React from 'react';
+import type { Profile } from '../profiles'; // Импорт интерфейса и данных
+ // Импорт интерфейса и данных
+import styles from './ProfileCard.module.css'; // Импорт стилей
 
 export default function ProfileCard({ profile }: { profile: Profile }) {
   return (
-    <div>
-      <img src={profile.avatarUrl} alt={`${profile.firstName} ${profile.lastName}`} />
-      <h2>{`${profile.firstName} ${profile.lastName}`}</h2>
-      <p>{profile.occupation}</p>
-      <h3>Hobbies: {profile.hobby}</h3>
+    <div className={styles.card}>
+      <img
+        className={styles.avatar}
+        src={profile.avatarUrl}
+        alt={`${profile.firstName} ${profile.lastName}`}
+      />
+      <h2 className={styles.name}>
+        {`${profile.firstName} ${profile.lastName}`}
+      </h2>
+      <p className={styles.occupation}>{profile.occupation}</p>
+      <h3 className={styles.hobby}>
+        Hobbies: <span className={styles.hobbyValue}>{profile.hobby}</span>
+      </h3>
     </div>
-  )
+  );
 }
